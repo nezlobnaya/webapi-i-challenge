ef7af280401511d4da9627a0e8630b28e7c72427
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-11-26 16:07:00 -0500

    a new /users endpoint created that returns the list of users

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db')
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+server.get('/hobbits', (req, res) => {
+  const hobbits = [
+    {
+      id: 1,
+      name: 'Samwise Gamgee',
+    },
+    {
+      id: 2,
+      name: 'Frodo Baggins',
+    },
+  ];
+
+  res.status(200).json(hobbits);
+});
+
+server.get('/users', (req, res) => {
+  db.find()
+  .then(users => {
+    res.json({ 
+      users })
+  })
+  .catch(error => {
+    res.status(500, 'The users information could not be retrieved', res)
+  })
+
+})
+
+server.listen(8000, () => console.log('API running on port 8000'));
